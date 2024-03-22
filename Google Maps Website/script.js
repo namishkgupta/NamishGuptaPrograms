df7fd599bdfb018d11e9053a8c97a6e3421d89b2
Namish Gupta <namishgupta@namishs-mbp.mynetworksettings.com>
2024-03-21 22:33:10 -0400

    Added Files

---
+++ Google Maps Website/script.js
@@ -0,0 +1,11 @@
+function calculatPosition() {
+    const origin = document.getElementById('origin').value;
+    const destination = document.getElementById('destination').value;
+    const state = document.getElementById('state').value; 
+    
+    const mapUrl = "https://maps.google.com/maps?q="+ origin + "%2C%20" + state + "&t=&z=13&ie=UTF8&iwloc=&output=embed";
+
+    const mapIframe = document.getElementById('map-iframe');
+
+    mapIframe.setAttribute('src', mapUrl);
+}
