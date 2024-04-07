cfdae33379b2e727c7de544a0b85c3e656e5339a
Namish Gupta <95318640+namishkgupta@users.noreply.github.com>
2024-04-07 18:41:14 -0400

    Update script.js

--- Carbon Emissions Tracker/script.js
+++ Carbon Emissions Tracker/script.js
@@ -1,10 +1,30 @@
-function calculatPosition() {
-    const origin = document.getElementById('origin').value;
-    const state = document.getElementById('state').value; 
-    
-    const mapUrl = "https://maps.google.com/maps?q="+ origin + "%2C%20" + state + "&t=&z=13&ie=UTF8&iwloc=&output=embed";
+function calculatePosition() {
+    const origin = document.getElementById('origin').value;    
+    const finalOrigin = format(origin)
+    const distOrigin = format2(origin)
+
+    const destination = document.getElementById('destination').value;    
+    const finalDestination = format(destination)
+    const distDestination = format2(destination)
+
+    const mapUrl = "https://www.google.com/maps/embed/v1/directions?key=AIzaSyBs5sPfD3BjUJQdTXo9vlyYZ5_LCb3ImyQ&&origin="+finalOrigin+"&destination="+finalDestination+"&avoid=tolls|highways";
 
     const mapIframe = document.getElementById('map-iframe');
 
     mapIframe.setAttribute('src', mapUrl);
+
+}
+
+function format(stringd){
+    const splitString = stringd.split(" ");
+    const joinedString = splitString.join("+");
+    return joinedString;
+
+}
+
+function format2(stringd){
+    const splitString = stringd.split(" ");
+    const joinedString = splitString.join("%20");
+    return joinedString;
+
 }
